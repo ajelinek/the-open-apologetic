472958ef556725b10b49d14fc31234922d141b6f
Adam Jelinek <ajelinek@gmail.com>
2026-02-24 19:41:06 -0600

    Remove deployment step from GitHub Actions and ensure base path is set in Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://the-open-apologetic.com',
+  base: '/the-open-apologetic',
   integrations: [sitemap()],
 });
