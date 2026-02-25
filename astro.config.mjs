256a5d8a6382439d4babfc0b7c9d9a75c7288c7d
Adam Jelinek <ajelinek@gmail.com>
2026-02-24 21:16:18 -0600

    added root path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ajelinek.github.io/the-open-apologetic-wks',
-  trailingSlash: 'always',
-  integrations: [sitemap()],
-});
+  base: '/the-open-apologetic',
+  integrations: [ sitemap() ],
+})
