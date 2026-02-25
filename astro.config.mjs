ad3c3e5020fdc981235a728d5847d00bc6ce0ce9
Adam Jelinek <ajelinek@gmail.com>
2026-02-25 05:29:01 -0600

    Refactor links to use base URL for consistent routing across the application

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://the-open-apologetic.com',
   base: '/the-open-apologetic',
   integrations: [ sitemap() ],
 })
